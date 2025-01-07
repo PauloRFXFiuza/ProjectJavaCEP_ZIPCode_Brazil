cc0aa80857069e6e3e280a98cb16e3a58e3d3b27
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-01-07 12:32:05 -0300

    v2.30

--- src/cep/Sobre.java
+++ src/cep/Sobre.java
@@ -37,6 +37,7 @@ public class Sobre extends JDialog {
 	 * Create the dialog.
 	 */
 	public Sobre() {
+		setModal(true);
 		getContentPane().setBackground(new Color(128, 128, 128));
 		getContentPane().setLayout(null);
 		
