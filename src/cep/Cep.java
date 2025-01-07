9a353dfe89cf02754a1613bd8ace02fc65b4e359
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-01-07 10:54:19 -0300

    v2.9

--- src/cep/Cep.java
+++ src/cep/Cep.java
@@ -9,6 +9,11 @@ import java.awt.Toolkit;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.SystemColor;
+import java.awt.Cursor;
+import javax.swing.ImageIcon;
+import java.awt.Color;
 
 public class Cep extends JFrame {
 
@@ -45,6 +50,7 @@ public class Cep extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(128, 128, 128));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
@@ -55,7 +61,7 @@ public class Cep extends JFrame {
 		contentPane.add(lblNewLabel);
 		
 		textField = new JTextField();
-		textField.setBounds(129, 21, 86, 20);
+		textField.setBounds(150, 21, 80, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
@@ -74,7 +80,7 @@ public class Cep extends JFrame {
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(129, 91, 277, 20);
+		textField_2.setBounds(150, 91, 256, 20);
 		contentPane.add(textField_2);
 		
 		JLabel lblCityCidade = new JLabel("UF");
@@ -83,7 +89,7 @@ public class Cep extends JFrame {
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
-		textField_3.setBounds(113, 128, 214, 20);
+		textField_3.setBounds(150, 128, 177, 20);
 		contentPane.add(textField_3);
 		
 		JLabel lblCityCidade_1 = new JLabel("City- Cidade");
@@ -93,5 +99,22 @@ public class Cep extends JFrame {
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(357, 127, 49, 22);
 		contentPane.add(comboBox);
+		
+		JButton btnNewButton = new JButton("Clean - Limpar");
+		btnNewButton.setBounds(25, 215, 130, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Search - Buscar");
+		btnNewButton_1.setBounds(276, 20, 130, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("");
+		btnNewButton_2.setToolTipText("About - Sobre");
+		btnNewButton_2.setIcon(new ImageIcon(Cep.class.getResource("/img/about.png")));
+		btnNewButton_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_2.setBorder(null);
+		btnNewButton_2.setBackground(new Color(128, 128, 128));
+		btnNewButton_2.setBounds(25, 156, 48, 48);
+		contentPane.add(btnNewButton_2);
 	}
 }
